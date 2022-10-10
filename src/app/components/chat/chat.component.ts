34a228b0b3ff9d840c2ae7dedf8de9066aea4cc9
steve <steve.lorent@bstorm.be>
2022-10-10 11:53:18 +0200

    first

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as signalr from '@microsoft/signalr';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+
+   url : string = "https://localhost:7263/chathub";
+  //url : string ="http://tbchat.somee.com/chathub";
+
+  hub! : signalr.HubConnection
+
+  messageList : Message[] = []
+  newmessage! : string
+  author! : string
+  constructor() { }
+
+  ngOnInit(): void {
+    //Création de l'objet permettant la connexion
+    this.hub = new signalr.HubConnectionBuilder().withUrl(this.url).build()
+
+    //Ouverture de la connexion avec gestion erreur evéntuelle
+    this.hub.start().then(() => console.log("Connexion Ok"))
+                    .catch((error) => console.log(error))
+
+    //Abonnnement à l'évènement "receiveMessage" + quoi faire quand il est déclenché
+    this.hub.on("receiveMessage", (data : Message) => this.messageList.push(data))
+  }
+
+  sendMessage() {
+    this.hub.send("SendMessage", {author :  this.author, content : this.newmessage})
+    this.newmessage = ""
+  }
+
+}
+//npm install @microsoft/signalr --save
+
+export interface Message {
+  author : string
+  content : string
+}
